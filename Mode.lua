5ac3573a1c32cc7401c1c929634280dbf3baea67
Tupo <denduhadelaetstrimy@gmail.com>
2025-02-01 13:38:55 +0300

    Update Mode.lua

--- Mode.lua
+++ Mode.lua
@@ -1,5 +1,11 @@
--- Spawner
+local storage = game.ReplicatedStorage
+local gamestats = storage.GameStats.Total
 
+multimSpawn = math.random(10, 15)
+
+-- Spawner and Achievements
+
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
 local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
 
 -- Sprint
@@ -36,9 +42,8 @@ game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.caption("v.0.1 Goo
 wait(2)
 game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.caption("Also press Shift to run.",true)
 
-wait(30)
-
-local entity = spawner.Create({
+if gamestats.DoorsOpened.Value = multimSpawn then
+	local entity = spawner.Create({
 	Entity = {
 		Name = "Multi-Matcher",
 		Asset = "https://github.com/Tupoi-Scripts/wrong_Hotel-Mode/blob/main/Entities/MultiMatcher.rbxm?raw=true",
@@ -84,8 +89,8 @@ local entity = spawner.Create({
 		Break = true
 	},
 	Death = {
-		Type = "Guiding", -- "Curious"
-		Hints = {"Death", "Hints", "Go", "Here"},
+		Type = "Curious", -- "Curious"
+		Hints = {"Oh, hi, don't ask me why I'm here.", "As I understand it, you died from the one we didn't meet.", "Let's call it... MultiMatcher", "I'm sorry, but I do not know how to escape from it."},
 		Cause = "MultiMatcher"
 	}
 })
@@ -129,125 +134,12 @@ entity:SetCallback("OnDespawning", function()
 end)
 
 entity:SetCallback("OnDespawned", function()
-    print("Entity has despawned")
-end)
-
-entity:SetCallback("OnDamagePlayer", function(newHealth)
-	if newHealth == 0 then
-		print("Entity has killed the player")
-	else
-		print("Entity has damaged the player")
-	end
-end)
-
---[[
-
-DEVELOPER NOTE:
-By overwriting 'CrucifixionOverwrite' the default crucifixion callback will be replaced with your custom callback.
-
-entity:SetCallback("CrucifixionOverwrite", function()
-    print("Custom crucifixion callback")
-end)
-
-]]--
-
----====== Run entity ======---
-
-entity:Run()
-wait(15)
-
-local entity = spawner.Create({
-	Entity = {
-		Name = "DepthMoving",
-		Asset = "https://github.com/Tupoi-Scripts/wrong_Hotel-Mode/blob/main/Entities/MultiMatcher.rbxm?raw=true",
-		HeightOffset = 0
-	},
-	Lights = {
-		Flicker = {
-			Enabled = true,
-			Duration = 5
-		},
-		Shatter = true,
-		Repair = false
-	},
-	Earthquake = {
-		Enabled = false
-	},
-	CameraShake = {
-		Enabled = true,
-		Range = 100,
-		Values = {1.5, 20, 0.1, 1} -- Magnitude, Roughness, FadeIn, FadeOut
-	},
-	Movement = {
-		Speed = 75,
-		Delay = 3,
-		Reversed = true
-	},
-	Rebounding = {
-		Enabled = true,
-		Type = "Ambush", -- "Blitz"
-		Min = 1,
-		Max = 2,
-		Delay = 3
-	},
-	Damage = {
-		Enabled = true,
-		Range = 40,
-		Amount = 125
-	},
-	Crucifixion = {
-		Enabled = true,
-		Range = 40,
-		Resist = false,
-		Break = true
-	},
-	Death = {
-		Type = "Guiding", -- "Curious"
-		Hints = {"Death", "Hints", "Go", "Here"},
-		Cause = ""
-	}
+    achievementGiver({
+    Title = "Lucky, lucky.. But I'll still be back.",
+    Desc = "Don't think it's over.",
+    Reason = "Encounter Multi-Matcher",
+    Image = "rbxassetid://0"
 })
-
----====== Debug entity ======---
-
-entity:SetCallback("OnSpawned", function()
-    print("Entity has spawned")
-end)
-
-entity:SetCallback("OnStartMoving", function()
-    print("Entity has started moving")
-end)
-
-entity:SetCallback("OnEnterRoom", function(room, firstTime)
-    if firstTime == true then
-        print("Entity has entered room: ".. room.Name.. " for the first time")
-    else
-        print("Entity has entered room: ".. room.Name.. " again")
-    end
-end)
-
-entity:SetCallback("OnLookAt", function(lineOfSight)
-	if lineOfSight == true then
-		print("Player is looking at entity")
-	else
-		print("Player view is obstructed by something")
-	end
-end)
-
-entity:SetCallback("OnRebounding", function(startOfRebound)
-    if startOfRebound == true then
-        print("Entity has started rebounding")
-	else
-        print("Entity has finished rebounding")
-	end
-end)
-
-entity:SetCallback("OnDespawning", function()
-    print("Entity is despawning")
-end)
-
-entity:SetCallback("OnDespawned", function()
-    print("Entity has despawned")
 end)
 
 entity:SetCallback("OnDamagePlayer", function(newHealth)
