da683268e4b323a45ac306a6b8bc3bb1d6378bfa
Tupo <denduhadelaetstrimy@gmail.com>
2025-02-01 15:13:56 +0300

    Update HotelMode.lua

--- HotelMode.lua
+++ HotelMode.lua
@@ -19,17 +19,26 @@ cc.Name = "ScriptedCC"
 cc.Contrast = 0.16
 cc.TintColor = Color3.fromRGB(243, 238, 255)
 
+local sound = game.ReplicatedStorage.Sounds
+
+sound.BulbBreak.SoundId = "rbxassetid://2612674531"
+sound.BulbZap.SoundId = "rbxassetid://4288784832"
+sound.BulbCharge.SoundId = "rbxassetid://166047422"
+
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Loaded Tupoi's 'Wrong Hotel' mode!", true)
-wait(1)
+wait(4)
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("v.0.1 Good Luck!", true)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/invisSprint.lua"))()
 
 local storage = game.ReplicatedStorage
 local gamestats = storage.GameStats.Total
 
 -- Spawner
 
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+
 if gamestats.DoorsOpened.Value == 10 then
-	local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
 	
 	local entity = spawner.Create({
 	Entity = {
