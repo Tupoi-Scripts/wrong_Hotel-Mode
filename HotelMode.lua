243bf47303ee1e12477a15c3a390552df1707547
Tupo <denduhadelaetstrimy@gmail.com>
2025-02-02 13:28:48 +0300

    Update HotelMode.lua

--- HotelMode.lua
+++ HotelMode.lua
@@ -16,24 +16,64 @@ dof.NearIntensity = 1
 local cc = Instance.new("ColorCorrectionEffect")
 cc.Parent = game.Lighting
 cc.Name = "ScriptedCC"
-cc.Contrast = 0.16
 cc.TintColor = Color3.fromRGB(243, 238, 255)
 
 local sound = game.ReplicatedStorage.Sounds
-
 sound.BulbBreak.SoundId = "rbxassetid://2612674531"
 sound.BulbZap.SoundId = "rbxassetid://4288784832"
 sound.BulbCharge.SoundId = "rbxassetid://166047422"
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/invisSprint.lua"))()
+
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Loaded Tupoi's 'Wrong Hotel' mode!", true)
 wait(4)
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("v.0.1 Good Luck!", true)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/invisSprint.lua"))()
+wait(4)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("LOSER IF YOUR EXECUTOR DONT HAVE 'require' LMAO-", true)
+wait(3)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Xeno executor...", true)
 
 local storage = game.ReplicatedStorage
 local gamestats = storage.GameStats.Total
 
--- Spawner
+-- MultiMatcher Spawn Setup
 
 if gamestats.DoorsOpened.Value == 10 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 17 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 19 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 25 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 36 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 40 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 48 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 53 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 60 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
+
+if gamestats.DoorsOpened.Value == 68 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Tupoi-Scripts/wrong_Hotel-Mode/refs/heads/main/MultiMatcher.lua"))()
+end
